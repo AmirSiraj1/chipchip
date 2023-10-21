9f85915f433692d29f05d58484ab1faed93a9544
Amir Siraj <amaressabit0314@gmail.com>
2023-10-21 13:18:59 +0300

    making the main file more clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
-import 'package:chipchip/utils/theme/theme.dart';
+import 'package:chipchip/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'chipchip',
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      home: const Scaffold(),
-    );
-  }
 }
---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:chipchip/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'chipchip',
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: const Scaffold(),
+    );
+  }
+}
